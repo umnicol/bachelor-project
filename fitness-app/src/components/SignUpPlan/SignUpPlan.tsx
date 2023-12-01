1c3c623f01202da850f4d98aef9a4d0739eccd2c
Lucia Supova <luciasupovaa@gmail.com>
2023-12-01 17:02:59 +0100

    plan

--- fitness-app/src/components/SignUpPlan/SignUpPlan.tsx
+++ fitness-app/src/components/SignUpPlan/SignUpPlan.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SignUpPlan.module.scss';
 import Button from '../Button/Button';
 
-
 const SignUpPlan: React.FC = () => {
+  const plans = [
+    { title: 'FREE', description: ['lorem ipsum'], monthlyPrice: 'FREE', yearlyPrice: 'FREE' },
+    { title: 'BASIC', description: ['lorem ipsum', 'lorem ipsum'], monthlyPrice: '59DKK', yearlyPrice: '590DKK' },
+    { title: 'PREMIUM', description: ['lorem ipsum', 'lorem ipsum', 'lorem ipsum'], monthlyPrice: '99DKK', yearlyPrice: '990DKK' },
+  ];
 
+  const [selectedPriceDuration, setSelectedPriceDuration] = useState<'monthly' | 'yearly'>('monthly');
+  const [selectedPlan, setSelectedPlan] = useState<string>('PREMIUM');
+  
+  const priceDurationChange = (priceDuration: 'monthly' | 'yearly') => {
+    setSelectedPriceDuration(priceDuration);
+  };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-
     // Perform authentication logic
   };
 
@@ -16,8 +25,45 @@ const SignUpPlan: React.FC = () => {
     <div className={styles.signupplanContainer}>
       <form onSubmit={handleSubmit}>
         <p>Step 2 of 3</p>
-      <h2>Choose your plan</h2>
-    
+        <h2>Choose your plan</h2>
+
+        <div className={styles.pricedurationSwitch}>
+          <button
+            className={`${styles.pricedurationButton} ${selectedPriceDuration === 'monthly' ? styles.selectedPriceDuration : ''}`}
+            onClick={() => priceDurationChange('monthly')}
+          >
+            Monthly
+          </button>
+          <button
+            className={`${styles.pricedurationButton} ${selectedPriceDuration === 'yearly' ? styles.selectedPriceDuration : ''}`}
+            onClick={() => priceDurationChange('yearly')}
+          >
+            Yearly 
+            <p>30% off</p>
+          </button>
+        </div>
+
+        {plans.map((plan, index) => (
+          <div key={index} className={`${styles.signupplanColumn} ${index === plans.length - 1 ? styles.lastColumn : ''}`}>
+            <h3
+            className={`${styles.planTitle} ${selectedPlan === plan.title ? styles.selectedPlan : ''}`}
+            onClick={() => setSelectedPlan(plan.title)}
+            >
+            {plan.title}
+            </h3>
+            <p>{selectedPriceDuration === 'monthly' ? plan.monthlyPrice : plan.yearlyPrice}</p>
+            <ul>
+              {plan.description.map((bullet, bulletIndex) => (
+                <li key={bulletIndex}>
+                  <span role="img" aria-label="check-mark">
+                    ✔
+                  </span>{' '}
+                  {bullet}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
 
         <Button label={'Next'} />
       </form>
